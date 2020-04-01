5636f72a0bce2ee16ba2b603b8de7b47c5e8c569
jennmez <jennmez@gmail.com>
2020-03-31 22:49:29 -0500

    notes are now appearing outside of container

--- client/components/NoteContainer.js
+++ client/components/NoteContainer.js
@@ -1,14 +1,27 @@
 import React, {useEffect, useRef, useState} from 'react'
-import {gsap, TimelineMax} from 'gsap'
+import {gsap, TimelineMax, TweenMax} from 'gsap'
 import Keyboard from './Keyboard'
-import {useDispatch, mapDispatch} from 'react-redux'
+import {useDispatch, mapDispatch, useSelector} from 'react-redux'
+import {fetchSingleSong} from '../store'
+import styled from 'styled-components'
 
 // using song data from backend
 // in backwards order, need to reverse
-const song = [48, 52, 50, 53, 48, 52]
+const song = [48, 52, 53, 55, 57]
+const reverseSong = song.reverse()
 
 const NoteContainer = props => {
+  // const dispatch = useDispatch()
+  // useEffect(() => {
+  //   dispatch(fetchSingleSong(props.match.params.id))
+  // }, [])
+
+  // const {song} = useSelector(state => {
+  //   return {song: state.song}
+  // })
+
   let refArray = song.map(element => useRef(null))
+  console.log(refArray)
   // let _48 = null
   // let _50 = useRef(null)
   // let _52 = useRef(null)
@@ -18,21 +31,28 @@ const NoteContainer = props => {
 
   const [clickAnimation, setClickAnimation] = useState()
 
-  let [tl] = useState(new TimelineMax({autoRemoveChildren: true, paused: true}))
+  let [tl] = useState(new TimelineMax({paused: true}))
+
+  useEffect(() => {
+    setCounter(counter + 1)
+    setNotes(song.slice(counter, 5 + counter))
+  }, [])
 
   useEffect(
     () => {
-      for (let i = 0; i < song.length; i++) {
+      // reverse the order of the refArray so most movement happens at the top of of the array. needs to be OUTSIDE the for loop
+      refArray.reverse()
+      for (let i = 0; i < song.length + 1; i++) {
         setClickAnimation(
           tl
             .to(refArray.slice(i, 4 + i), 1, {
               ease: 'none',
-              top: 100 * i
+              top: 100 * (i + 1)
             })
-            // .to(_52, 1, {ease: 'none', top: 100})
-            // .to(_50, 1, {ease: 'none', top: 100})
-            // .to(_53, 1, {ease: 'none', top: 100})
             .addPause()
+          // .to(_52, 1, {ease: 'none', top: 100})
+          // .to(_50, 1, {ease: 'none', top: 100})
+          // .to(_53, 1, {ease: 'none', top: 100})
         )
       }
       // setClickAnimation(
@@ -46,8 +66,7 @@ const NoteContainer = props => {
       //     // .to(_53, 1, {ease: 'none', top: 100})
       //     .addPause()
       // )
-      setCounter(counter + 1)
-      setNotes(song.slice(counter, 4 + counter))
+
       // setClickAnimation(
       //   tl
       //     .to(refArray, 1, {ease: 'none', top: 200})
@@ -73,7 +92,6 @@ const NoteContainer = props => {
       // .to(_50, 1, {ease: 'none', top: 400})
       // .to(_53, 1, {ease: 'none', top: 400})
       // .addPause()
-      console.log('notes', currentNotes)
     },
     [tl]
   )
@@ -89,20 +107,22 @@ const NoteContainer = props => {
         </button>
       </div>
       <div className="fixed">
-        <div className="noteContainer">
-          {song.map((element, idx) => (
+        <NoteContainerStyle>
+          {/* <div className="noteContainer"> */}
+          {reverseSong.map((element, idx) => (
             <div
               key={`${idx}`}
               className={`_${element}`}
               ref={el => (refArray[idx] = el)}
             />
           ))}
-          {/* <div className="_48" ref={el => (_48 = el)} />
+          {/*
           <div className="_53" ref={el => (_53 = el)} />
           <div className="_52" ref={el => (_52 = el)} />
           <div className="_50" ref={el => (_50 = el)} />
-          <div className="_52" ref={el => (_52 = el)} /> */}
-        </div>
+          <div className="_52" ref={el => (_52 = el)} /> <div className="_48" ref={el => (_48 = el)} />*/}
+          {/* </div> */}
+        </NoteContainerStyle>
       </div>
       <Keyboard />
     </>
@@ -110,3 +130,13 @@ const NoteContainer = props => {
 }
 
 export default NoteContainer
+
+const NoteContainerStyle = styled.div`
+width: 100%;
+position: absolute;
+vertical-align: baseline;
+bottom: ${song.length * 100 - 400}px;
+max-height: 400px;
+border: 1px solid black;
+}
+`
